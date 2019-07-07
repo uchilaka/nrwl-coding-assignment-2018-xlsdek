df26c770df883c92c2ff2c775de042f875b26f39
Uche Chilaka <uche@lar.city>
2019-07-07 06:20:09 -0400

    (test) unit test to ticket-list.component

--- src/app/feature-tickets/ticket-list/ticket-list.component.spec.ts
+++ src/app/feature-tickets/ticket-list/ticket-list.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { provideMockStore } from "@ngrx/store/testing";
+import { provideMockStore, MockStore } from "@ngrx/store/testing";
 import { TicketListComponent } from "./ticket-list.component";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TicketEntityState, initialTicketEntityState } from "../models";
@@ -7,10 +7,15 @@ import { BackendService } from "../../backend.service";
 import { TicketListItemComponent } from "../ticket-list-item/ticket-list-item.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { Store } from "@ngrx/store";
+import { getTickets } from "../+store/ticket.selectors";
+import { By } from "@angular/platform-browser";
 
 describe("TicketListComponent", () => {
   let component: TicketListComponent;
   let fixture: ComponentFixture<TicketListComponent>;
+  let service: BackendService;
+  let store: MockStore<TicketEntityState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -26,6 +31,11 @@ describe("TicketListComponent", () => {
   }));
 
   beforeEach(() => {
+    service = TestBed.get(BackendService);
+    store = TestBed.get(Store);
+
+    store.overrideSelector(getTickets, service.storedTickets);
+
     fixture = TestBed.createComponent(TicketListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -33,5 +43,12 @@ describe("TicketListComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should return expected # of tickets", () => {
+    const matches = fixture.debugElement.queryAll(
+      By.directive(TicketListItemComponent)
+    );
+    expect(matches.length).toBe(service.storedTickets.length);
   });
 });
